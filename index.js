40d5ab7f50b1e5d1b9175d91d5c7d6373ec1b2c5
Brett Colyer <brettc911@gmail.com>
2017-10-18 03:47:58 -0400

    dfdf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -22,8 +23,6 @@ var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -58,9 +57,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-app.listen(app.get('port'), () => {
-  console.log('app running');
-})
 
 module.exports = app;
