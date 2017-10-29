92427a4aee8fb8352c4e379b7b4ba5b837ae2049
Brett Colyer <brettc911@gmail.com>
2017-10-29 14:20:06 -0400

    slkdjflsj

--- models/card.js
+++ models/card.js
@@ -2,24 +2,6 @@ const mongoose = require('mongoose');
 
 var CardSchema = new mongoose.Schema({
 
-  cardName: { type: String, requried: true },
-  primaryType: { type: String, requried: true },
-  subType: { type: Array, default: [] }
-  // manaCost: { type: String, requried: true },
-  // action: { type: String, requried: true },
-  // castRange: { type: String, requried: true },
-  // target: { type: String },
-  // schools: [{ name: String, level: String  }],
-  // armor: { type: Number },
-  // health: { type: Number },
-  // defense: { type: String },
-  // equipmentSlot: { type: Array },
-  // abilities: [
-  //   {name: { type: String }, action: { type: String }, attackType: { type: String }, range: { type: String }, dice: { type: Number }, effect: { type: Boolean }, effectDescription: { type: String }, attackModifier: { type: Array } },
-  //   {name: { type: String }, action: { type: String }, attackType: { type: String }, range: { type: String }, dice: { type: Number }, effect: { type: Boolean }, effectDescription: { type: String }, attackModifier: { type: Array } },
-  //   {name: { type: String }, action: { type: String }, attackType: { type: String }, range: { type: String }, dice: { type: Number }, effect: { type: Boolean }, effectDescription: { type: String }, attackModifier: { type: Array } }
-  // ],
-  // description: { type: String },
 })
 
 
