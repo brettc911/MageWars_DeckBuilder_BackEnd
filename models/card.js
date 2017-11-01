e5a266ab2676e51b7a34e4f4288d76b057a0d97b
Brett Colyer <brettc911@gmail.com>
2017-11-01 18:00:42 -0400

    ddd

--- models/card.js
+++ models/card.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 
 var CardSchema = new mongoose.Schema({
 
+  cardName: { type: String, requried: true },
+  primaryType: { type: String, requried: true },
+  subTypes: { type: Array},
+  manaCost: { type: String, requried: true },
+  action: { type: String, requried: true },
+  castRange: { type: String, requried: true },
+  target: { type: String },
+  schools: { type: Array },
+  armor: { type: Number },
+  health: { type: Number },
+  defense: { type: String },
+  channeling: { type: String },
+  equipmentSlot: { type: String },
+  abilities: { type: Array },
+  traits: { type: Array },
+  details: { type: String },
 })
 
 
