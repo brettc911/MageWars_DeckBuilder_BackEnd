cce6a3fc8365dac850f1a3d7ffd1e7ab981a4b5f
Brett Colyer <brettc911@gmail.com>
2017-11-21 00:36:42 -0500

    fixed bug that was a syntax error

--- models/deck.js
+++ models/deck.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 var DeckSchema = new mongoose.Schema({
 
   deckName: { type: String, requried: true },
-  mage: { type: String }
-  cards: { type: Array },
+  mage: { type: String },
+  cards: { type: Array }
 })
 
 
