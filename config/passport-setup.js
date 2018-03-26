6866941755c88640b80ff8d64ba42b8e9d8b08bc
Brett Colyer <brettc911@gmail.com>
2018-03-26 09:24:32 -0400

    removed file

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -7,8 +7,6 @@ passport.use(
     callbackURL: '/auth/google/redirect',
     // clientID: process.env.CLIENT_ID,
     // clientSecret: process.env.CLIENT_SECRET
-    clientID: '110239888022-1jm74fe2p01b5rjj3fem7f4c7qrc5l4e.apps.googleusercontent.com',
-    clientSecret: '7oppScddalfSlVQHt2HWKa7a'
   }, function(accessToken, refreshToken, profile, done) {
 
     // check if user already exists
